f25beedac6ae0345242417b70f2ccce71015d0eb
SalinasJason <jasonsalinas770@gmail.com>
2025-02-10 18:53:30 -0800

    Added Step 6: Instantiate and Use the Classes

--- COMP003A.LectureActivity5-3/Program.cs
+++ COMP003A.LectureActivity5-3/Program.cs
@@ -109,7 +109,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Create a circle and a rectangle
+            Shape myCircle = new Circle(5);
+            // Create a rectangle 
+            Shape myRectangle = new Rectangle(4, 6);
+
+            // Display information about the circle
+            myCircle.DisplayInfo();
+            Console.WriteLine($"Area: {myCircle.CalculateArea()}");
+            ((IDrawable)myCircle).Draw();
+
+            // Display information about the rectangle
+            myRectangle.DisplayInfo();
+            Console.WriteLine($"Area: {myRectangle.CalculateArea()}");
+            ((IDrawable)myRectangle).Draw();
         }
     }
 }
