b4503621ba4529e768c0d77c6bf0c1167d1adcdb
SalinasJason <jasonsalinas770@gmail.com>
2025-02-10 17:33:18 -0800

    Add project files.

---
+++ COMP003A.LectureActivity5-3/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace COMP003A.LectureActivity5_3
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
